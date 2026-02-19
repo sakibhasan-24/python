276ffdbe946904c237dad5baae7461e2fb2102af
sakibhasan-24 <sakibhasan7724@gmail.com>
2026-02-20 00:39:08 +0600

    loops

--- DayOne/calculator.py
+++ DayOne/calculator.py
@@ -16,3 +16,37 @@ elif op=="/":
     print(numOne/numTwo)
 else:
     print("Invalid operator")
+
+
+
+#Logical Operators
+
+# and, or, not
+# and - both conditions must be true
+# or - either one of the conditions must be true
+# not - reverse the result
+# True and True = True
+# True and False = False
+# False and True = False
+# False and False = False
+
+
+role =input("Enter your role: (admin, user, guest) ")
+isAccessCard=True
+userExperience=4
+
+if role =="admin" and isAccessCard and userExperience>3:
+    print ("Welcome admin")
+    print("You can apply for project Manager...........")
+elif role =="user" and isAccessCard and userExperience>2:
+    print ("Welcome user")
+    print("You can apply for developer...........")
+elif role =="guest" and isAccessCard and userExperience>1:
+    print ("Welcome guest")
+    print("You can apply for tester...........")
+
+
+isLoggedIn=False
+
+if not isLoggedIn:
+    print("Please login to continue")
---
+++ loop.py
@@ -0,0 +1,17 @@
+#For Loop
+
+
+for i in range(1,10):
+   print(f"number is  :  {i}")
+
+
+
+
+#while loop
+
+age=int(input("Enter your age : "))
+while age<0:
+    print("Invalid age")
+    age=int(input("Enter your age : "))
+
+print(f"age is {age}")
