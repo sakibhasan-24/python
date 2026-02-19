77965598deabe4063d30903cca4a89336f52e6c6
sakibhasan-24 <sakibhasan7724@gmail.com>
2026-02-19 13:48:47 +0600

    basic variable and type casting

--- DayOne/basicOne.py
+++ DayOne/basicOne.py
@@ -1,1 +1,20 @@
 print("restart a new journey to check a person's avility")
+
+# variable
+
+
+name ="Sakib"
+age = 35
+isMale = True
+height = 5.11
+weight = 70.5
+
+print(name,age,isMale,height,weight)
+
+# type casting
+
+print(type(name))
+print(type(age))
+print(type(isMale))
+print(type(height))
+print(type(weight))
