4d61dd6ea7fb4fa5c9d3470f6e8798895740cee8
sakibhasan-24 <sakibhasan7724@gmail.com>
2026-02-26 22:22:49 +0600

    access data from a dictionary

--- DayOne/dictionary.py
+++ DayOne/dictionary.py
@@ -24,5 +24,27 @@ print(person)
 # print(person)
 
 
-for key ,value in person.items():
-    print(f"{key} : {value}")
+# for key ,value in person.items():
+    # print(f"{key} : {value}")
+
+
+
+
+students = {
+    "101": {
+        "name": "Rahim",
+        "age": 21,
+        "marks": {"math": 85, "english": 78, "cs": 90},
+        "attendance": 88
+    },
+    "102": {
+        "name": "Karim",
+        "age": 22,
+        "marks": {"math": 70, "english": 60, "cs": 75},
+        "attendance": 72
+    }
+}
+
+
+# access data
+print(students["101"]["name"])
