a7a7774f6491b9d3d9fb0ff634da87db7ff36c1c
sakibhasan-24 <sakibhasan7724@gmail.com>
2026-03-02 00:28:57 +0600

    function with parameter

--- DayTwo/function.py
+++ DayTwo/function.py
@@ -5,3 +5,20 @@ def make_coffee():
 print("starting")
 make_coffee()
 print("ending")
+
+
+# def clean_text():
+#     name=" SakiB  "
+#     print(name.strip().lower())
+
+
+# clean_text()
+
+
+def clean_text(name):
+    print(name.strip().lower().capitalize())
+
+
+clean_text(" sakiB  ")
+clean_text("  SakiB  ")
+clean_text("  BangladeSh  ")
