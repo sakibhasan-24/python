700046eff3ef6fcb1f56dc050879839eff5d027b
sakibhasan-24 <sakibhasan7724@gmail.com>
2026-03-03 00:51:43 +0600

    **kwargs arguments

--- DayTwo/function.py
+++ DayTwo/function.py
@@ -62,3 +62,34 @@ def  power(num, power=2):
 
 print(power(2,3))
 print(power(2))
+
+
+
+# args vs kwargs
+# args means arguments 
+
+# *args allows function to accept unlimited positional arguments
+
+def sum_all(*args):
+    # print(*args)
+    for c in args:
+        if type(c) is not int:
+            print("Please enter only integer")
+            return
+    total=0
+    for num in args:
+        total+=num
+    print(total)
+sum_all(1,2,3,4,5,6,7)
+
+
+
+# *kwargs allows function to accept unlimited keyword arguments
+
+def create_user(**kwargs):
+    print(kwargs)
+    print(kwargs["name"])
+    print(kwargs["age"])
+
+create_user(name="John", age=25)
+create_user(name="Jane", age=30, city="New York", country="USA", email="jane@gmail.com")
