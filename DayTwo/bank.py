fdb6d38d0ad076a2c9b1bdb0ae6c5bb8c4cc8d42
sakibhasan-24 <sakibhasan7724@gmail.com>
2026-03-03 22:31:02 +0600

    solve banking logic using function

--- DayTwo/bank.py
+++ DayTwo/bank.py
@@ -1,7 +1,33 @@
 
 balance=0
 is_processing=True
+def show_balance():
+      print(f"Your balance is ${balance:.2f}")
 
+def deposit():
+    global balance
+    amount=float(input("Enter amount to deposit: "))
+    if amount > 0:
+        balance += amount
+        print(f"Your balance is now ${balance:.2f}")
+        return amount
+    else:
+        print("Invalid amount")
+
+def withdraw():
+    global balance
+    if balance< 0:
+        print("Insufficient balance")
+        return
+    else:
+        amount=float(input("Enter amount to withdraw: "))
+        if amount > balance:
+            print("Insufficient balance")
+            return
+        else:
+            balance -= amount
+            print(f"Your balance is now ${balance:.2f}")
+            return amount
 while is_processing:
     print("1. Deposit")
     print("2. Withdraw")
@@ -10,11 +36,12 @@ while is_processing:
     choice =(input("Enter your choice: "))
 
     if choice == "1":
-        print("deposit")
+        
+        deposit()
     elif choice == "2":
-        print("withdraw")
+       withdraw()
     elif choice == "3":
-        print(f"Your balance is ${balance:.2f}")
+        show_balance()    
     elif choice == "4":
         is_processing = False
     else:
