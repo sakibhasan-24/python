caa8cb3384cef7c904d4b9b1845fb2eb9dd1e366
sakibhasan-24 <sakibhasan7724@gmail.com>
2026-03-04 23:53:41 +0600

    adding method

--- DayThreeOOP/oopBasic.py
+++ DayThreeOOP/oopBasic.py
@@ -10,13 +10,21 @@
 class Person:
     # adding properties in class ,we called it attribute
     def __init__(self,name,email,age):
+        # __init__ is a constructor method that runs automatically when an object is created
+# self is a reference to the current instance of the class 
+# and is used to access variables that belongs to the class
         # print(self)
         self.name = name
         self.email = email
         self.age = age
+
+    # method
+    def greet(self):
+        return f"Hello {self.name} , you are {self.age} years old"
 
 
 # now create an object of class
 
 person1 = Person("sakib","sakib@gmail.com",30)
 print (person1.age)
+print(person1.greet())
